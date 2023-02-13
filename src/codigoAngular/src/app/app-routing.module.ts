a9d60b53db414d6db3ddfea1079aadc3c7cead64
arvodb <daboma@floridauniversitaria.com>
2023-02-13 21:32:51 +0100

    preparing dashboard

--- src/codigoAngular/src/app/app-routing.module.ts
+++ src/codigoAngular/src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { DashboardUserComponent } from './views/dashboard-user/dashboard-user.component';
-import {  NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { MainUserComponent } from './components/main-user/main-user.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'dashboard', pathMatch: 'full'},
-  /* {path:'/login', component: AppComponent}, */
-   {path:'dashboard',component: DashboardUserComponent}
+  {path:'dashboard',component: DashboardUserComponent}
 ];
 
 @NgModule({
