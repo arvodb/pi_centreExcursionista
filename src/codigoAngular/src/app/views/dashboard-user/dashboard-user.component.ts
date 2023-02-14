06827336f9d26aab72d7dd9c736159113ba32ec8
arvodb <daboma@floridauniversitaria.es>
2023-02-14 12:56:15 +0100

    Añadiendo modals

--- src/codigoAngular/src/app/views/dashboard-user/dashboard-user.component.ts
+++ src/codigoAngular/src/app/views/dashboard-user/dashboard-user.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-user.component.css']
 })
 export class DashboardUserComponent {
-
+  public calendarNums : number[][] = [];
+  public colsCalendar : number = 7
+  public fillCalendar() : void {
+    let num = 30;
+    let array = [];
+    let row = [];
+    for (let i = 1; i <= num; i++) {
+      row.push(i);
+      if (row.length === this.colsCalendar || i === num) {
+        array.push(row);
+        row = [];
+      }
+    }
+    this.calendarNums = array;
+  }
+  ngOnInit(){
+    this.fillCalendar();
+  }
 }
+
+
