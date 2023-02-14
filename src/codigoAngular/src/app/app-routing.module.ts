2b809018ad73b9166b1086415ee9010cbc134b82
arvodb <daboma@floridauniversitaria.com>
2023-02-14 19:13:35 +0100

    dashboard structure finished, have to take a look for responsive design

--- src/codigoAngular/src/app/app-routing.module.ts
+++ src/codigoAngular/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { DashboardUserComponent } from './views/dashboard-user/dashboard-user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookingMaterialComponent } from './views/booking-material/booking-material.component';
 
 
 const routes: Routes = [
   {path:'', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path:'dashboard',component: DashboardUserComponent}
+  {path:'dashboard',component: DashboardUserComponent},
+  {path:'bookingMaterial',component: BookingMaterialComponent}
 ];
 
 @NgModule({
